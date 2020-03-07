451abf0b98f7645d9655c071aceb306f82c9d5e5
rlaw125 <12490147+rlaw125@users.noreply.github.com>
2020-03-06 20:07:00 -0500

    added listener

--- index.js
+++ index.js
@@ -2,4 +2,15 @@ const express=require('express');
 const app=express();
 const basicAuth=require('express-basic-auth');
 const scrypt=require('scrypt-js');
-let port=process.env.PORT ||
+const pg=require('pg');
+const bodyParser=require('body-parser');
+let port=process.env.PORT || 3000;
+
+
+app.get('/', (req,res)=>{
+  res.send('Hello');
+});
+
+app.listen(port, (req, res)=>{
+  console.log('Listening on port ' + port);
+});
